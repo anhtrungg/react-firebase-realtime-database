23dd717cc313b58efe784f2208778c4d78897537
anhtrungg <anhtrung.work@gmail.com>
2022-11-08 15:50:14 +0700

    Updates

---
+++ precache-manifest.7e428f325be526209a8decb539faee1c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "378d5aa0156967304704",
+    "url": "/react-firebase-realtime-database/static/js/main.378d5aa0.chunk.js"
+  },
+  {
+    "revision": "4a1aa8a6eb06d972f90e",
+    "url": "/react-firebase-realtime-database/static/css/1.e8fc90fc.chunk.css"
+  },
+  {
+    "revision": "4a1aa8a6eb06d972f90e",
+    "url": "/react-firebase-realtime-database/static/js/1.4a1aa8a6.chunk.js"
+  },
+  {
+    "revision": "5f4ef1a77d088f6d89f2",
+    "url": "/react-firebase-realtime-database/static/js/runtime~main.5f4ef1a7.js"
+  },
+  {
+    "revision": "b2d8346caa8f3947d6309a7a8b5c8c11",
+    "url": "/react-firebase-realtime-database/index.html"
+  }
+];
